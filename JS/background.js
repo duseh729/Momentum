5e5d5f16991fed0eaa19984a438ee8ebad7c2643
DoYeon Ahn <duseh729@naver.com>
2023-04-17 14:57:19 +0900

    fix: 깃허브 페이지에서 아미지 안뜨는 오류 수정

--- JS/background.js
+++ JS/background.js
@@ -2,4 +2,4 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('../IMG/${chosenImage}')`;
+document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('./IMG/${chosenImage}')`;
